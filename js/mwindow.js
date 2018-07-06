5d3067e1733dc9ac15cea2ad07daf13bcd0d04bd
Lucas Malvasia <lucas.malvasia@gmail.com>
2018-07-05 22:25:42 -0300

    Input validation(wip)

--- js/mwindow.js
+++ js/mwindow.js
@@ -4,19 +4,37 @@ var Window = {
   getAccept: function() {
     return document.getElementsByName('Accept')[0];
   },
-  Accept: function() {
-    Window.accept = Window.getAccept();
-    Window.accept.onclick = Window.getNumberFromHTML;
+  getNumberFromInput: function() {
+    return document.getElementsByName('cells')[0].value;
+  },
+  createDivValidation: function() {
+    var formCells = document.getElementsByClassName('formCells')[0]; // Div with class formCells
+    var Validation = document.createElement("div"); // New node, element div
+    Validation.className = 'validation';
+    var parentDiv = formCells.parentNode; // Parent of formCells
+    parentDiv.insertBefore(Validation, formCells); // Add node validation to HTML
+    Validation.innerHTML = 'Ingrese un <strong>&nbsp;"numero"&nbsp;</strong> entre <strong>&nbsp;"0 y 25"&nbsp;</strong>'
   },
   hideModalWindow: function() {
     document.getElementsByClassName('modalwindow')[0].style.display = 'none';
   },
-  getNumberFromHTML: function() {
-    Window.number = document.getElementsByName('cells')[0].value;
-    Window.hideModalWindow();
-    Board.rows = Window.number;
-    Board.columns = Window.number;
-    Game.init();
-    Game.start();
+  Accept: function() {
+    Window.accept = Window.getAccept();
+    Window.accept.onclick = Window.setBoardDimension;
+  },
+  setBoardDimension: function() {
+    Window.number = Window.getNumberFromInput();
+    if (Window.number >= 5 && Window.number <= 25) {
+      Window.hideModalWindow();
+      Board.rows = Window.number;
+      Board.columns = Window.number;
+      Game.init();
+      Game.start();
+    } else {
+      if (document.getElementsByClassName('validation')[0] === undefined) {
+        Window.createDivValidation();
+
+      }
+    }
   }
 }
